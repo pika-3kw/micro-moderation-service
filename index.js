02dab3cd7c5ab501da411f8099f8f2d22fc42597
pika-3kw <giangnam10a2@gmail.com>
2021-06-20 21:30:41 +0700

    Moderation Comment

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4000/events", {
+      type: "CommentModerated",
+      data: { ...data, status },
+    });
+  }
+
+  res.json({});
+});
+
+app.listen(4004, () => {
+  console.log("Moderation Service running at 4004");
+});
